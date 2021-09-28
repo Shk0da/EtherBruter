9589daa5760830bdc5214e1b3c546e07daf36b2e
Anatoly.Shkondin <anatoly.shkondin@nordigy.ru>
2021-09-28 19:46:11 +0300

    upd

--- src/main/java/com/github/shk0da/crypto/Crypto.java
+++ src/main/java/com/github/shk0da/crypto/Crypto.java
@@ -2,6 +2,7 @@ package com.github.shk0da.crypto;
 
 import org.web3j.crypto.CipherException;
 import org.web3j.crypto.Credentials;
+import org.web3j.crypto.MnemonicUtils;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
@@ -10,7 +11,6 @@ import org.web3j.protocol.http.HttpService;
 
 import java.io.File;
 import java.io.FileDescriptor;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -19,12 +19,8 @@ import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.MathContext;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.security.SecureRandom;
 import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
 import java.util.TimeZone;
 import java.util.concurrent.ExecutionException;
 
@@ -32,13 +28,14 @@ import static java.lang.System.out;
 import static java.lang.System.setOut;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.TimeZone.setDefault;
-import static java.util.concurrent.ThreadLocalRandom.current;
 
 public class Crypto {
 
     private static final File walletsDir = new File("wallets");
     private static final String ETHEREUM_NODE_LOCAL = "http://localhost:8545";
     private static final Web3j WEB3 = Web3j.build(new HttpService(ETHEREUM_NODE_LOCAL));
+
+    private static final SecureRandom secureRandom = new SecureRandom();
 
     static {
         if (!walletsDir.exists()) {
@@ -51,19 +48,14 @@ public class Crypto {
     public static void main(String[] args) {
         setDefault(TimeZone.getTimeZone("Europe/Moscow"));
         setOut(new PrintStream(new FileOutputStream(FileDescriptor.out), true, UTF_8));
-        out.printf("%s: Start Crypto%n", new Date());
+        out.printf("%s: Start Crypto%n%n", new Date());
 
         try {
             out.printf("Current Gas price: %s%n", getGasPrice());
-            var alphabet = readAlphabet();
             while (true) {
-                StringBuilder phrase = new StringBuilder();
-                for (int i = 0; i < 12; i++) {
-                    int random = current().nextInt(0, alphabet.length);
-                    phrase.append(alphabet[random]);
-                    phrase.append(" ");
-                }
-                var mnemonic = phrase.toString().trim();
+                byte[] initialEntropy = new byte[16];
+                secureRandom.nextBytes(initialEntropy);
+                String mnemonic = MnemonicUtils.generateMnemonic(initialEntropy);
                 out.println("mnemonic: " + mnemonic);
                 checkWallet(mnemonic);
                 out.println();
@@ -74,23 +66,6 @@ public class Crypto {
             ex.printStackTrace();
         }
         out.printf("%s: Finish Crypto%n", new Date());
-    }
-
-    public static String[] readAlphabet() throws IOException {
-        List<String> words = new ArrayList<>(500);
-        try (FileInputStream inputStream = new FileInputStream("alphabet/words.txt");
-             Scanner sc = new Scanner(inputStream, StandardCharsets.UTF_8)) {
-            while (sc.hasNextLine()) {
-                String line = sc.nextLine().toLowerCase(Locale.ROOT);
-                if (line.length() >= 4 && line.length() <= 9 && line.chars().allMatch(Character::isLetter)) {
-                    words.add(line);
-                }
-            }
-            if (sc.ioException() != null) {
-                throw sc.ioException();
-            }
-        }
-        return words.toArray(new String[]{});
     }
 
     private static void checkWallet(String mnemonic) throws InterruptedException, ExecutionException, IOException, CipherException {
